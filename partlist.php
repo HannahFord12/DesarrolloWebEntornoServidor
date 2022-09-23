96b6aed6e2142f8fdbf1f94fce52520b93436900
Davcer99 <davidbalaguercervrea@gmail.com>
2022-09-23 12:09:07 +0200

    Ejercicio partlist

--- partlist.php
+++ partlist.php
@@ -1,9 +1,11 @@
 <?php
     $palabras = ["Seguro", "que" , "apruebo", "esta", "asignatura"];
-
-    for ($i=0; $i < (count($palabras)-1); $i++) { 
-        $resultado[$i]= array_slice($palabras,$i+1,count($palabras));
-        $resultado[$i] = array_slice($palabras,$i+1);
+    $iterator= 0;
+    for ($i=0; $i < (count($palabras)-1); $i++) {   
+    $resultado[$i][$iterator]= implode(" ", array_slice($palabras,0,$i+1));
+    $iterator++;
+    $resultado[$i] [$iterator]= implode(" ", array_slice($palabras,$i+1));  
+    $iterator++;
     };
     print_r($resultado);
 ?>
