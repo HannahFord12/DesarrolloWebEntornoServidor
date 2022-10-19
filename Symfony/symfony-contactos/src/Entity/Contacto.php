dc2bb96c9be2bba4502d4e951c1b35636603efdf
Davcer99 <davidbalaguercervera@gmail.com>
2022-10-19 10:39:41 +0200

    Ejercicios hechos en clase

--- Symfony/symfony-contactos/src/Entity/Contacto.php
+++ Symfony/symfony-contactos/src/Entity/Contacto.php
@@ -3,12 +3,12 @@
 namespace App\Entity;
 
 use App\Repository\ContactoRepository;
-use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @ORM\Entity(repositoryClass=ContactoRepository::class)
  */
-
 class Contacto
 {
     /**
@@ -16,72 +16,83 @@ class Contacto
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-
     private $id;
-    /**
-     * @ORM\Column(type="string", length=255)
-     * @Assert\NotBlank
-     * (message="El nombre es obligatorio")
-     */
 
-    private $nombre;
-    /**
-     * @ORM\Column(type="string", length=15)
-     * @Assert\NotBlank
-     * (message="El teléfono es obligatorio")
-     */
-
-    private $telefono;
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     * @Assert\NotBlank()
+     */
+    private $telefono;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Email()
      * @Assert\Email(message="El email {{ value }} no es válido")
      */
-
     private $email;
+
     /**
      * @ORM\ManyToOne(targetEntity=Provincia::class)
      */
-
     private $provincia;
 
-    public function getId(): ?int{
+    public function getId(): ?int
+    {
         return $this->id;
     }
 
-    public function getNombre(): ?string{
+    public function getNombre(): ?string
+    {
         return $this->nombre;
     }
 
-    public function setNombre(?string $nombre): self{
+    public function setNombre(string $nombre): self
+    {
         $this->nombre = $nombre;
+
         return $this;
     }
 
-    public function getTelefono(): ?string{
+    public function getTelefono(): ?string
+    {
         return $this->telefono;
     }
-    
-    public function setTelefono(?string $telefono): self{
+
+    public function setTelefono(string $telefono): self
+    {
         $this->telefono = $telefono;
+
         return $this;
     }
 
-    public function getEmail(): ?string{
+    public function getEmail(): ?string
+    {
         return $this->email;
     }
 
-    public function setEmail(?string $email): self{
+    public function setEmail(string $email): self
+    {
         $this->email = $email;
+
         return $this;
     }
 
-    public function getProvincia(): ?Provincia{
+    public function getProvincia(): ?Provincia
+    {
         return $this->provincia;
     }
-    
-    public function setProvincia(?Provincia $provincia): self{
+
+    public function setProvincia(?Provincia $provincia): self
+    {
         $this->provincia = $provincia;
+
         return $this;
     }
 }
