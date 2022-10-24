38eee9ac740f3f1725c9afcb8f9ab8719586bf1c
Davcer99 <davidbalaguercervera@gmail.com>
2022-10-24 13:54:20 +0200

    Correcion

--- Symfony/symfony-productos/src/Controller/TelefonosController.php
+++ Symfony/symfony-productos/src/Controller/TelefonosController.php
@@ -40,29 +40,5 @@ class TelefonosController extends AbstractController
         return $this->render('nuevo.html.twig',array(
             'formulario'=>  $formulario->createView()
         ));
-    }
-
-    #[Route('/telefonos/insertar', name: 'insertar_telefono')]
-    public function insertar(ManagerRegistry $doctrine){
-        $entityManager = $doctrine->getManager();
-        foreach ($this->telefonos as $c) {
-            $telefono = new Telefonos();
-            $telefono ->setMarca($c["marca"]);
-            $telefono ->setModelo($c["modelo"]);
-            $telefono ->setPrecio($c["precio"]);
-            $telefono ->setProcesador($c["procesador"]);
-            $telefono ->setRam($c["ram"]);
-            $telefono ->setAlmacenamiento($c["almacenamiento"]);
-            $entityManager ->persist($telefono);
-        }
-        try{
-            //solo se necesitara hacer flush una vez y confirmara todas las operaciones pendientes
-            $entityManager->flush();
-            return new Response("Telefonos insertados ");
-        }catch (\Exception $e){
-            return new Response("Error al insertar objetos" . $e->getMessage());
-        }
-        
-        
-    }
+    }    
 }
