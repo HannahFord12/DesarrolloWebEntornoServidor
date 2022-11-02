7729f6479a6c36f7c2d7d4346d9b42a944cebb0f
Davcer99 <davidbalaguercervera@gmail.com>
2022-11-02 09:27:04 +0100

    Ejercicios blog

--- Symfony/symfony-blog/src/Controller/PageController.php
+++ Symfony/symfony-blog/src/Controller/PageController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,9 +31,21 @@ class PageController extends AbstractController
     }
 
     #[Route('/contact', name: 'contact')]
-    public function contact(): Response
+    public function contact(ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('page/contact.html.twig', []);
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contacto = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($contacto);
+            $entityManager->flush();
+            return $this->redirectToRoute('thankyou', []);
+        }
+        return $this->render('page/contact.html.twig', array(
+            'form' => $form->createView()    
+        ));
     }
 
     #[Route('/singlePost', name: 'singlePost')]
