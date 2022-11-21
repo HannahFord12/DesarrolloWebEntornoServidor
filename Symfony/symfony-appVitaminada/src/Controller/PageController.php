a372bd18ad293879d196ef20201cf8903d9db107
Davcer99 <davidbalaguercervera@gmail.com>
2022-11-21 13:52:16 +0100

    Mi app plantilla pasada

--- Symfony/symfony-appVitaminada/src/Controller/PageController.php
+++ Symfony/symfony-appVitaminada/src/Controller/PageController.php
@@ -8,11 +8,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'incio')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
+        return $this->render('page/index.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig');
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig');
+    }
+
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        return $this->render('page/blog.html.twig');
+    }
+
+    #[Route('/postDetails', name: 'postDetails')]
+    public function postDetails(): Response
+    {
+        return $this->render('page/postDetails.html.twig');
     }
 }
