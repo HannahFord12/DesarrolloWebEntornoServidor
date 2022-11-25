f5d80b7d04883bc818ec495a587dfea86d12c851
Davcer99 <davidbalaguercervera@gmail.com>
2022-11-25 13:43:03 +0100

    Añadir new blog a mi app vitaminada

--- Symfony/symfony-appVitaminada/src/Controller/BlogController.php
+++ Symfony/symfony-appVitaminada/src/Controller/BlogController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostFormType;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
+
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -26,6 +28,28 @@ class BlogController extends AbstractController
         $form= $this->createForm(PostFormType::class,$post);
         $form -> handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
+            $file = $form->get('image')->getData();
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+        
+                // Move the file to the directory where images are stored
+                try {
+        
+                    $file->move(
+                        $this->getParameter('images_directory'), $newFilename
+                    );
+        
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+        
+                // updates the 'file$filename' property to store the PDF file name
+                // instead of its contents
+                $post->setImage($newFilename);
+            }
             $post = $form->getData();
             $post->setSlug($slugger->slug($post->getTitle()));
             $post->setPostUser($this->getUser());
@@ -33,9 +57,14 @@ class BlogController extends AbstractController
             $entityManager = $doctrine->getManager();
             $entityManager->persist($post);
             $entityManager->flush();
+            return $this->render('blog/new_post.html.twig',array(
+                'form' => $form->createView()
+            ));
             
 
         }
-        return $this->render('page/blog.html.twig');
+        return $this->render('blog/new_post.html.twig', array(
+            'form' => $form->createView()    
+        ));
     }
 }
--- Symfony/symfony-appVitaminada/src/Form/PostFormType.php
+++ Symfony/symfony-appVitaminada/src/Form/PostFormType.php
@@ -6,6 +6,8 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PostFormType extends AbstractType
@@ -13,10 +15,21 @@ class PostFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('image')
-            ->add('send', SubmitType::class)
+            ->add('title',  null, ['attr' => ['class'=>'form-control']])
+            ->add('content',  null, ['attr' => ['class'=>'form-control']])
+            ->add('image',FileType::class,[
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes'=>[
+                            'image/jpeg',
+                            'image/png'
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file'
+                    ])
+                ]
+            ])
+            ->add('send', SubmitType::class, array('label' => 'Send', 'attr' => ['class'=>'btn btn-lg']))
         ;
     }
 
