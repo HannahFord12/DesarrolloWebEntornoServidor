48f8163358b26d778cbe92895e0f19ff1c3f6b73
Davcer99 <davidbalaguercervera@gmail.com>
2023-01-26 11:22:15 +0100

    Adelantado en clase

--- Symfony/whatsapp-clone-skeleton-main/public/js/mensaje.js
+++ Symfony/whatsapp-clone-skeleton-main/public/js/mensaje.js
@@ -1,11 +1,12 @@
+var currentId;
 function sendMessage (event){
-    const url = "/message/3"
+    const url = `/post/touser/${currentId}`;
     if (event.keyCode === 13) {
         $.get(url)
     }
-}
+} 
 
-/* $(document).ready(function(){
+/*$(document).ready(function(){
     const url = "./app_page"
     $("#send-message").on("keypress", function(){
         if (this.keyCode === 13) {
@@ -13,3 +14,26 @@ function sendMessage (event){
         }
     })
 }) */
+$(document).ready(function(){
+    var url=`/contact`;
+    $.get(url, function(data){
+        data.forEach(function(contacto){
+            createDOMContact(contacto);
+        })
+    })
+})
+
+//Pintar contactos
+const contacts =  document.getElementById("contacts"); //div donde vamos a pintar los contactos
+const templateContact = document.getElementById("templateContact").innerHTML;
+
+function createDOMContact(m){
+    var el = document.createElement('span');
+    el.innerHTML = templateContact;
+    el.getElementsByClassName("contact.userName")[0].innerHTML = m.username;
+    el.getElementsByClassName("contact")[0].setAttribute("data-id", m.id);
+    el.getElementsByClassName("contact.info")[0].innerHTML = m.info;
+    el.getElementsByClassName("contact.image")[0].src=( "/img/" + m.image);
+    contacts.appendChild(el);
+    //Ya solo te falta añadir los eventos clic
+}
--- Symfony/whatsapp-clone-skeleton-main/src/Controller/PageController.php
+++ Symfony/whatsapp-clone-skeleton-main/src/Controller/PageController.php
@@ -3,34 +3,26 @@
 namespace App\Controller;
 
 use App\Entity\Message;
+use App\Entity\User;
 use App\Form\MessageType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'app_page')]
-    public function index(ManagerRegistry $doctrine, Request $request): Response
+    #[Route('/', name: 'app_page', methods: ['GET'])]
+    public function index(ManagerRegistry $doctrine): Response
     {
         $message = new Message();
         $form = $this->createForm(MessageType::class, $message);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $message = $form->getData();
-            $message->setPostUser($this->getUser());
-            $entityManager = $doctrine->getManager();
-            $entityManager->persist($message);
-            $entityManager->flush();
-            return $this->render('page/index.html.twig');
-        }
         return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
             'form'=> $form->createView()
-    
-        ]);
+        ]); 
+
     }
     #[Route('/message/{toUserId}', name: 'send-message')]
     public function message(ManagerRegistry $doctrine, int $toUserId, Request $request): Response
@@ -46,6 +38,38 @@ class PageController extends AbstractController
             $entityManager->persist($message);
             $entityManager->flush();
         }
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+            'form'=> $form->createView()
+    
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact',  methods: ['GET'])]
+    public function contact(ManagerRegistry $doctrine): JsonResponse
+    {
+        $repository = $doctrine->getRepository(User::class);
+        $users = $repository->findAll();
+        if (!$users)
+            return new JsonResponse("[]", Response::HTTP_NOT_FOUND);
+        $data = [];
+        foreach ($users as $user) {
+            if ($this->getUser()->getId() == $user->getId())
+                continue;
+            $item = [
+
+                "id" => $user->getId(),
+                "username" => $user->getUsername(),
+                "info" => $user->getInfo(),
+                "image" => $user->getImage()
+                
+            ];
+            $data[] = $item;
+        }    
+        
+        
+        return new JsonResponse($data, Response::HTTP_OK);
+    
         
     }
 }
